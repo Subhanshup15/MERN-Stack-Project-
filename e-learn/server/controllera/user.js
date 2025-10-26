c1b526a3b88d2bdad06e59d11c48227685105c76
Subhanshu Pardeshi <subhanshup.15@gmail.com>
2025-10-26 16:07:01 +0530

    --add email send form

--- e-learn/server/controllera/user.js
+++ e-learn/server/controllera/user.js
@@ -1,30 +1,43 @@
 import { json } from "express";
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-  try {
-   const {email,name,password} = req.body
-   let user =await User.findOne({email});
-   if(user){
+    try {
+        const { email, name, password } = req.body
+        let user = await User.findOne({ email });
+        if (user) {
 
-    return res.status(400),json({
-        message:"user exist",
-    });
+            return res.status(400), json({
+                message: "user exist",
+            });
 
-    const hashPassword  = await bcrypt.hash(password,10)
-  user ={
-     name,
-     email,
-     password:hashPassword
-  }
+            const hashPassword = await bcrypt.hash(password, 10)
+            user = {
+                name,
+                email,
+                password: hashPassword
+            }
 
-  const otp = Math.floor(Math.random()*1000000);
-  const
+            const otp = Math.floor(Math.random() * 1000000);
+            const activationToken = jwt.sign({
 
-   }
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
+                user,
+                otp,
+            }, process.env.Activation_Secret, {
+                expiresIn: "5m",
+            }
+            );
+            const data ={
+                name,
+                otp,
+            };
+
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
 };
